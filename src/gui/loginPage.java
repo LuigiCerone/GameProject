7c592fe54d3827982f43b9830dcb0d9ec8f05bdb
Luigi Cerone <luigiceroneaq@gmail.com>
2017-05-23 19:51:00 +0200

    Revert "update"
    This reverts commit 7ca87b87f42ddf02647f0374bc5d8ad4664d3df4.

--- src/gui/loginPage.java
+++ src/gui/loginPage.java
@@ -1,7 +1,3 @@
-/*
- * @author Cerone Luigi
- * @version 1.0
- * */
 package gui;
 
 import java.awt.BorderLayout;
@@ -10,30 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import controller.loginController;
-import database.Dbms;
-import model.Utente;
-import view.loginView;
-
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JPasswordField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class loginPage extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
-	private JLabel lblNonSeiAncora;
-	private JLabel labelErrore;
 
 	/**
 	 * Launch the application.
@@ -55,70 +34,20 @@ public class loginPage extends JFrame {
 	 * Create the frame.
 	 */
 	public loginPage() {
-		loginPage lP = this;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 550, 350);
-		this.setResizable(false);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JButton btnAccedi = new JButton("Accedi");
-		btnAccedi.setBounds(300, 181, 126, 40);
-		btnAccedi.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		
-		JLabel lblUsername = new JLabel("Username :");
-		lblUsername.setBounds(63, 32, 117, 30);
-		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		
-		textField = new JTextField();
-		textField.setToolTipText("username");
-		textField.setBounds(232, 32, 162, 29);
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		textField.setColumns(10);
-		
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(63, 105, 78, 23);
-		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		
-		passwordField = new JPasswordField();
-		passwordField.setToolTipText("password");
-		passwordField.setBounds(232, 102, 162, 29);
-		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		
-		lblNonSeiAncora = new JLabel("Non sei ancora registrato?");
-		lblNonSeiAncora.setBounds(94, 273, 175, 14);
-		
-		JButton btnRegistrati = new JButton("Registrati");
-		btnRegistrati.setBounds(300, 256, 126, 40);
-		btnRegistrati.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		contentPane.setLayout(null);
-		contentPane.add(lblNonSeiAncora);
-		contentPane.add(btnRegistrati);
-		contentPane.add(lblUsername);
-		contentPane.add(lblPassword);
-		contentPane.add(passwordField);
-		contentPane.add(textField);
-		contentPane.add(btnAccedi);
-		
-		labelErrore = new JLabel("Errore! Username/Password errati");
-		labelErrore.setVisible(false);
-		labelErrore.setEnabled(false);
-		labelErrore.setBounds(206, 142, 246, 14);
-		contentPane.add(labelErrore);
-		
 		btnAccedi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new loginView().accedi(textField.getText(), new String(passwordField.getPassword()), labelErrore,lP);
 			}
 		});
-		
-		btnRegistrati.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new loginView().registrati(lP);
-			}
-		});
+		btnAccedi.setBounds(294, 93, 89, 43);
+		contentPane.add(btnAccedi);
 	}
-	
-	
+
 }
--- src/model/dao/UtenteDAO_Interface.java
+++
@@ -1,9 +0,0 @@
-package model.dao;
-
-import java.sql.SQLException;
-
-public interface UtenteDAO_Interface {
-	public boolean ËRegistrato();
-	
-	public Object tipoUtente(String user, String password) throws SQLException;
-}
